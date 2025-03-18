0c6d64c92f9d1369de9a59390e3a3a8c81e54193
mn <eaglel4rk@gmail.com>
2025-03-18 17:29:49 +0900

    ユーザー登録実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
 
-  has_many :rooms
+  validates :name, presence: true
+
+  has_many :rooms, dependent: :destroy
 end
