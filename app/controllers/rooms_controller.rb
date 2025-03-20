cf7b6257fa9542b3f4a98b32145305d6c564a05d
mn <eaglel4rk@gmail.com>
2025-03-20 17:00:08 +0900

    room 編集、削除機能実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -11,18 +11,40 @@ class RoomsController < ApplicationController
   end
 
   def create
+    @room = Room.new(room_params)
+    if @room.save
+      flash[:info] = "作成しました！"
+      redirect_to room_path(@room.id)
+    else
+      flash.now[:error] = "失敗しました"
+      render :new
+    end
   end
 
   def show
+    @room = Room.find(params[:id])
   end
-
+  
   def edit
+    @room = Room.find(params[:id])
   end
 
   def update
+    @room = Room.find(params[:id])
+    if @room.update(room_params)
+      flash[:info] = "更新しました"
+      redirect_to room_path(@room.id)
+    else
+      falsh.now[:error] = "失敗しました"
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @room = Room.find(params[:id])
+    @room.destroy!
+    flash[:info] = "施設情報を削除しました"
+    redirect_to rooms_path, status: :see_other
   end
 
   private
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,14 @@
 class Room < ApplicationRecord
   has_many :users
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :fee, presence: true
+
+  validate :check_fee_minimum
+
+  def check_fee_minimum
+    errors.add(:fee, "金額は1円以上に設定してください") if fee.nil? || fee < 1
+  end
+
 end
