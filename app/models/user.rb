2ff0f96dcfa13a1baa55f9cf134fd897e1ded5d0
mn <eaglel4rk@gmail.com>
2025-03-25 23:41:17 +0900

    予約画面のデザイン実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
 
   has_many :reservations, dependent: :destroy
   has_many :rooms, through: :reservations
-  has_many :rooms
   before_destroy :destroy_rooms
   has_one_attached :avater
   
